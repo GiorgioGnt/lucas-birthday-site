722449181f29b95138d7f56c9c4ed153fc7f8d8b
Giorgio Giantomassi <giorgio_giantomassi@yahoo.com>
2026-05-04 22:21:41 -0400

    Create first bday site

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main style={{ fontFamily: "Arial", padding: "40px", textAlign: "center" }}>
+      <h1>🎉 Lucas is turning 5! 🎉</h1>
+      <p>You are invited to my birthday party!</p>
+      <p>More details coming soon...</p>
+    </main>
+  );
+}
+
+export default App;
